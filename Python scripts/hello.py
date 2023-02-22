e544ed10222b5c61730a4216fd49a46354392d6d
GouravSalla <35566779+GouravSalla1213@users.noreply.github.com>
2023-02-22 22:31:02 +0530

    Add files via upload

---
+++ Python scripts/hello.py
@@ -0,0 +1,22 @@
+#python
+import sys
+print("Welcome to Pyhton practice with sys module")
+name=input("Pls enter your name:")
+print("Hai " + name + " welcome !")
+print("you are using :" , sys.version ," of Python")
+print(sys.argv)
+print("Enter another name: ")
+name2 = sys.stdin.readline()
+print(name2)
+print("Enter a 2 digit number :")
+num = sys.stdin.readline(2)
+print(num)
+#print(sys.stdin)
+#print(sys.stdout)
+'''
+import os
+import subprocess
+import math
+import random
+import json
+'''
